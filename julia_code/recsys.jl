e58ae491debe72add43dc27eb7a994eeadccadd3
Arni Sturluson <sturlusa@oregonstate.edu>
2019-08-27 11:58:02 -0700

    Adjustments to julia code

--- julia_code/recsys.jl
+++ julia_code/recsys.jl
@@ -2,25 +2,26 @@ using PyPlot
 using CSV
 using DataFrames
 using Printf
+using JSON
 using Statistics
 using LinearAlgebra
 include("recsys_funcs.jl")
 
 
-henry_df = CSV.read(joinpath("..","data","henry_matrix_df.csv"))
+henry_df = CSV.read(joinpath("..","data","henry_matrix_df_l.csv"))
 gases = names(henry_df)[2:end]
 materials = henry_df[:,1]
 
 H = convert(Array{Union{Float64, Missing}, 2}, henry_df[2:end, 2:end])
-H[83,8] = missing
+#H[83,8] = missing
 log_H = log10.(H)
 #M, G, train_error = ALS(log_H, 5, [2.0, 0.01], 1e-5, maxiter=50000)
 
 fold_matrix = k_fold_split(log_H, 3)
 
-_r = [4,5]
-_λ₁ = [0.1, 1.0, 2.0]
-_λ₂ = [0.01, 0.1, 1.0]
+_r = [4]
+_λ₁ = [2.0]
+_λ₂ = [1.0]
 n_cv = 0
 CV_dict = Dict{AbstractString, Any}()
 
@@ -28,8 +29,8 @@ for r in _r
     for λ₁ in _λ₁
         for λ₂ in _λ₂
             global n_cv += 1
-#            test_error, parity_pred = cross_validation(log_H, fold_matrix, r, λ₁, λ₂)
-            test_error, parity_pred = LOO_cross_validation(log_H, r, λ₁, λ₂)
+            test_error, parity_pred = cross_validation(log_H, fold_matrix, r, λ₁, λ₂)
+#            test_error, parity_pred = LOO_cross_validation(log_H, r, λ₁, λ₂)
             CV_dict[string(n_cv) * "_err"] = test_error
             CV_dict[string(n_cv) * "_pred"] = parity_pred
             CV_dict[string(n_cv) * "_lambda1"] = λ₁
@@ -39,3 +40,7 @@ for r in _r
     end
 end
 
+json_string = JSON.json(CV_dict)
+outfile = open("CV_dict_drasl.json", "w")
+print(outfile, json_string)
+close(outfile)
