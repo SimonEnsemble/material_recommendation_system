9b134203058c519488eed636bf059b333df9d53e
SimonEnsemble <cory.simon@oregonstate.edu>
2021-01-25 19:00:01 -0800

    theta 0.4 study

--- cof_rec_sys.jl
+++ cof_rec_sys.jl
@@ -6,8 +6,9 @@ using InteractiveUtils
 
 # ╔═╡ 92083d94-5b82-11eb-2274-ed62139bbf2d
 begin
-	using LowRankModels, CSV, DataFrames, PyPlot, Statistics, Distributions, StatsBase
+	using LowRankModels, CSV, DataFrames, PyPlot, Statistics, Distributions, StatsBase, Printf, UMAP
 	using ScikitLearn.CrossValidation: train_test_split
+	PyPlot.matplotlib.style.use("https://gist.githubusercontent.com/JonnyCBB/c464d302fefce4722fe6cf5f461114ea/raw/64a78942d3f7b4b5054902f2cee84213eaff872f/matplotlibrc")
 end
 
 # ╔═╡ ae415fa4-5b82-11eb-0051-072097bb0439
@@ -69,7 +70,7 @@ begin
 	                     "ch4_5.8bar"     => L"CH$_4$" * "\n(298 K, 5.8 bar)"
 	                    )
 
-	const materials = df[:, :cof]
+	const materials = map(x -> split(x, ".cif")[1], df[:, :cof])
 	const n_m = length(materials)
 	const n_p = length(properties)
 	# material property matrix
@@ -100,7 +101,7 @@ begin
 				if i == j
 					axs[i, j].set_ylim(hist_ylims)
 					axs[i, j].hist(A_n[:, i], 
-						           ec="k", fc="#fabebe", alpha=0.7)
+						           ec="k", fc="C0", alpha=0.7)
 					if i == 1
 						axs[i, j].set_ylabel("# COFs")
 					else
@@ -115,8 +116,10 @@ begin
 					end
 				# scatter plot
 				else
+					# axs[i, j].spines["left"].set_visible(true)
+					# axs[i, j].spines["bottom"].set_visible(true)
 					axs[i, j].scatter(A_n[:, j], A_n[:, i], 
-						              ec="k", fc="#fabebe", alpha=0.7)
+						              ec="k", fc="C0", alpha=0.7)
 					if i == n_p
 						axs[i, j].set_xlabel(property_j)
 					else
@@ -184,6 +187,39 @@ function normalize!(A::Array{Union{Float64, Missing}, 2})
 	return μs, σs
 end
 
+# ╔═╡ 6236b296-5f60-11eb-3aa7-5188433b3906
+md"# matrix viz"
+
+# ╔═╡ 5ae47630-5f64-11eb-39f8-654f8d277674
+function viz_matrix(A::Array{Union{Float64, Missing}, 2})
+	norm = PyPlot.matplotlib.colors.Normalize(vmin=-4.0, vmax=4.0)
+	cmap = PyPlot.matplotlib.cm.get_cmap("PiYG") # diverging
+	
+	# mapping adsorption properties to colors
+	function a_to_color(a::Union{Float64, Missing})
+		if ismissing(a)
+			return (0.5, 0.5, 0.5, 1.0)
+		else
+			return cmap(norm(a))
+		end
+	end
+	
+	figure(figsize=(4, 15))
+	ax = gca()
+	is = imshow(a_to_color.(A))
+	xlabel("gas\nadsorption\nproperties")
+	ylabel("COFs")
+	xticks([])
+	yticks([])
+	ylim([-0.5, size(A)[1]-0.5])
+	xlim([-0.5, size(A)[2]-0.5])
+	colorbar(PyPlot.matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap),
+		label=L"$A_{ij}$ (normalized)", extend="both", shrink=0.4)
+	tight_layout()
+	savefig("matrix_example.pdf", format="pdf")
+	gcf()
+end
+
 # ╔═╡ 6ef474cc-5b90-11eb-2fe2-87bc7b48f5b7
 md"# fit low rank model"
 
@@ -238,14 +274,21 @@ function fit_glrm(At::Array{Union{Float64, Missing}, 2},
 	# A = M' P
 	# At = P' M
 	#   so here X is the analogy of P; Y is the analogy of M
-    glrm = GLRM(At, QuadLoss(), rp, rm, hp.k, obs=obs, offset=true)
+    glrm = GLRM(At, QuadLoss(), rp, rm, hp.k+1, obs=obs, offset=true)
 #    init_svd!(glrm)
     P, M, ch = fit!(glrm)
-    @assert size(P) == (hp.k, n_p)
-    @assert size(M) == (hp.k, n_m)
+    @assert size(P) == (hp.k + 1, n_p)
+    @assert size(M) == (hp.k + 1, n_m)
 
     @assert isapprox(impute(glrm), P' * M)
     return P, M, glrm, ch
+end
+
+# ╔═╡ 21995e36-5f69-11eb-0a95-13d0136099df
+function fit_bias_only_glrm(At::Array{Union{Float64, Missing}, 2},
+		                     obs::Array{Tuple{Int64,Int64}, 1})
+	hp = HyperParam(0, 0.0)
+	return fit_glrm(At, hp, obs)
 end
 
 # ╔═╡ e2dd554c-5baf-11eb-1b49-654d19bedecc
@@ -289,8 +332,8 @@ md"# hyperparam grid sweep"
 
 # ╔═╡ a269ab26-5ba4-11eb-1001-6703f57f495c
 begin
-	ks = collect(2:3)                       # ranks
-	λs = 10.0 .^ range(-1.0, 3.0, length=3) # regularization params
+	ks = collect(1:5)                       # ranks
+	λs = 10.0 .^ range(-1.0, 3.0, length=5) # regularization params
 	hyper_params = hp_grid(ks, λs)
 end
 
@@ -305,7 +348,7 @@ function hyperparam_sweep(hyper_params::Array{HyperParam, 1},
 	valid_runs = ValidationRun[]
 	for hyper_param in hyper_params
 		# train on training data
-		G, M, glrm, ch = fit_glrm(At, hyper_param, ids_train)
+		G, P, glrm, ch = fit_glrm(At, hyper_param, ids_train)
 		# impute missing entries
 		Ât = impute(glrm)
 		# evaluate on validation data
@@ -325,69 +368,260 @@ end
 md"# performance evaluation"
 
 # ╔═╡ 09b74408-5baa-11eb-3735-e9333756f565
-struct TestResult
-	# gas-wise Spearman rank coeffs
-	ρs::Array{Float64, 1}
+struct Result
+	# the data
+	A::Array{Union{Float64, Missing}, 2}
+	# the depolyment model model
+	M::Array{Float64, 2}
+	P::Array{Float64, 2}
+	hp::HyperParam
 	# true test data pts
 	a::Array{Float64, 1}
 	# pred test data pts
 	â::Array{Float64, 1}
+	# pred test data for bias only
+	âb::Array{Float64, 1}
+	# gas-wise Spearman rank coeffs
+	ρp::Array{Float64, 1}
+	# gas-wise Spearman rank coeffs bias only
+	ρpb::Array{Float64, 1}
 end
 
 # ╔═╡ b285e2fe-5ba7-11eb-2e12-83e72bcafa2f
+# 1. simulate data collection
+# 2. split observed values into train and validation set
+# 3. remaining unobserved = test data
+# 4. do hyperparam sweep to find optimal hyperparams, judged by validation data
+# 5. now that we hv optimal hyper params, retrain on all observed.
+# 6. evaluate it on test entries
+#   return (true values, pred values)
+function run_simulation(θ::Float64)
+	###
+	#    set up data
+	###
+	A, θ_true = sim_data_collection(θ)
+
+	# store for later.
+	μs, σs = normalize!(A)
+
+	At = collect(A')
+	
+	# split into test/validation/train
+	ids_obs, ids_unobs = ids_obs_and_unobs(At)
+	ids_test = ids_unobs
+	@assert(all(ismissing.([At[p, m] for (p, m) in ids_test])))
+	ids_train, ids_valid = train_test_split(ids_obs, test_size=0.2, shuffle=true)
+
+	###
+	#   hyper-parameter sweep using train/valid data 
+	#        to find optimal hyper params (k, λ)
+	###
+	opt_valid_run = hyperparam_sweep(hyper_params, At, ids_train, ids_valid)
+
+	###
+	#   deployment time: train model on all observed data
+	###
+	P, M, glrm, ch = fit_glrm(At, opt_valid_run.hyper_param, ids_obs)
+	Gb, Mb, glrmb, chb = fit_bias_only_glrm(At, ids_obs)
+	
+	###
+	#   test time: test depolyed model on unobserved entries
+	###
+	Ât = impute(glrm)
+	Âtb = impute(glrmb)
+
+	# compute truth from normalizations
+	At_complete = compute_At_complete(μs, σs)
+
+	# test model on unobserved
+	a = [At_complete[p, m] for (p, m) in ids_test]
+	â = [Ât[p, m] for (p, m) in ids_test]
+	âb = [Âtb[p, m] for (p, m) in ids_test]
+	# get spearman ranks for gases
+
+	ρp = [ρ_p(p, At_complete, Ât, ids_test) for p = 1:n_p]
+	ρpb = [ρ_p(p, At_complete, Âtb, ids_test) for p = 1:n_p]
+	return Result(A, M, P, opt_valid_run.hyper_param,
+		          a, â, âb, ρp, ρpb)
+end
+
+# ╔═╡ 5d38b414-5f41-11eb-14b9-73a9007fc263
+md"# $\theta=0.4$ example"
+
+# ╔═╡ 4f81a520-5f6d-11eb-1960-9918ca4f25e9
+res = run_simulation(0.4)
+
+# ╔═╡ 68fe93ae-5f6e-11eb-012a-81378cd15b41
+viz_matrix(res.A)
+
+# ╔═╡ 53585188-5f6f-11eb-0fc0-abbd20ee33fe
 begin
-	# 1. simulate data collection
-	# 2. split observed values into train and validation set
-	# 3. remaining unobserved = test data
-	# 4. do hyperparam sweep to find optimal hyperparams, judged by validation data
-	# 5. now that we hv optimal hyper params, retrain on all observed.
-	# 6. evaluate it on test entries
-	#   return (true values, pred values)
-	function run_simulation(θ::Float64)
-		A, θ_true = sim_data_collection(θ)
-		
-		# store for later.
-		μs, σs = normalize!(A)
-
-		At = collect(A')
-
-		ids_obs, ids_unobs = ids_obs_and_unobs(At)
-		ids_test = ids_unobs
-		@assert(all(ismissing.([At[p, m] for (p, m) in ids_test])))
-		ids_train, ids_valid = train_test_split(ids_obs, test_size=0.2, shuffle=true)
-
-		# hyper-parameter sweep to find optimal hyper param
-		opt_valid_run = hyperparam_sweep(hyper_params, At, ids_train, ids_valid)
-
-		# train model on all observed
-		G, M, glrm, ch = fit_glrm(At, opt_valid_run.hyper_param, ids_obs)
-		
-		# impute unobserved entries
-		Ât = impute(glrm)
-		
-		# compute truth from normalizations
-		At_complete = compute_At_complete(μs, σs)
-		
-		# test model on unobserved
-		a = [At_complete[p, m] for (p, m) in ids_test]
-		â = [Ât[p, m] for (p, m) in ids_test]
-
-		# get spearman ranks for gases
-		
-		ρs = [ρ_p(p, At_complete, Ât, ids_test) for p = 1:n_p]
-		return TestResult(ρs, a, â)
+	# clip values
+	da_cutoff = 6.0
+	δ = 0.1
+	for 🐶 in [res.a, res.â]
+		🐶[🐶 .< - da_cutoff] .= - da_cutoff
+		🐶[🐶 .>   da_cutoff] .=   da_cutoff
 	end
 	
-	θ = 0.6
-	test_result = run_simulation(θ)
-end
-
-# ╔═╡ a6b959aa-5bb2-11eb-3ab4-eb90f14f6f61
-begin
+	# parity plot
 	figure()
-	scatter(test_result.a, test_result.â)
+	hexbin(res.a, res.â, mincnt=1, bins="log")
+	xlabel(L"true $A_{ij}$ (normalized)")
+	ylabel(L"predicted $A_{ij}$ (normalized)")
+	xlim([-da_cutoff - δ, da_cutoff + δ])
+	ylim([-da_cutoff - δ, da_cutoff + δ])
+	plot([-da_cutoff, da_cutoff], [-da_cutoff, da_cutoff], 
+		linestyle="--", color="gray")
+	text(4, -4, 
+		@sprintf("hyperparameters:\nk = %d\nλ = %.2f\n\nperformance:\nρ = %.2f\nRMSE = %.2f", res.hp.k, res.hp.λ, corspearman(res.a, res.â), rmsd(res.a, res.â)),
+		ha="center", va="center"
+		)
+	colorbar(label="# (COF, adsorption property) pairs")
+	gca().set_aspect("equal", "box")
+	tight_layout()
+	savefig("parity_plot.pdf", format="pdf")
 	gcf()
 end
+
+# ╔═╡ 74068408-5f70-11eb-02ba-417e847034c4
+begin
+	figure(figsize=(10, 4.8))
+	bar(1:n_p, res.ρp, label=@sprintf("k = %d, λ = %.2f", res.hp.k, res.hp.λ))
+	scatter(1:n_p, res.ρpb, marker="*", zorder=100, s=150, 
+		    ec="k", label="k = 0"
+		    )
+	xticks(1:n_p, [prop_to_label[p] for p in properties], rotation=90)
+	ylabel("Spearman's rank\ncorrelation coefficient\n"  * L"$\rho$")
+	# text(12.0, 0.9,
+	# 	@sprintf("hyperparameters:\nk = %d\nλ = %.2f", res.hp.k, res.hp.λ),
+	# 	ha="center", va="center"
+	# 	)
+	legend()
+	ylim([0.0, 1.0])
+	gcf()
+end
+
+# ╔═╡ 8548a48c-5f73-11eb-3d4f-550078ec546a
+begin
+	μ = res.M[end, :]
+	ids_sort = sortperm(μ, rev=true)
+	
+	n_show = 20
+	n_space = 3
+	
+	ids_best  = 1:n_show
+	ids_worst = (n_show + n_space + 1):(2 * n_show + n_space + 1)
+	
+	figure(figsize=(10.0, 4.8))
+	bar(ids_best , μ[ids_sort][1:n_show])
+	bar(ids_worst, μ[ids_sort][end-n_show:end])
+	xticks(vcat(ids_best, ids_worst), 
+		   vcat(materials[ids_sort][ids_best], materials[ids_sort][ids_worst]),
+		   rotation=90
+		  )
+	xlim([0, 2 * n_show + n_space + 2])
+	# ylim([-4.25, 4.25])
+	xlabel("COF")
+	ylabel(L"material bias, $\mu_i$")
+	
+	scatter((n_show+1):(n_show+n_space), zeros(3), color="k")
+	
+	tight_layout()
+	savefig("material_bias.pdf", format="pdf")
+	gcf()
+end
+
+# ╔═╡ b0560c02-5f80-11eb-338b-c9cc48b741af
+md"### learn latent space
+
+of materials and properties together.
+"
+
+# ╔═╡ ba8ce81e-5f80-11eb-3e39-f942cb6d0d1f
+X = hcat(res.M[1:end-1, :], res.P[1:end-1, :])
+
+# ╔═╡ c6caaa48-5f7f-11eb-3853-fdffcd51b2d5
+begin
+	# input: (a column-major matrix of shape (n_features, n_samples))
+	latent_space = umap(X, 2)
+	m_vecs = latent_space[:, 1:n_m]
+	p_vecs = latent_space[:, (n_m+1):end]
+	@assert size(p_vecs) == (2, n_p)
+end
+
+# ╔═╡ 59a72a22-5f82-11eb-1424-0913e7830bc4
+begin
+	figure()
+	scatter(m_vecs[1, :], m_vecs[2, :])
+	xlabel("UMAP dimension 1")
+	ylabel("UMAP dimension 2")
+	gca().set_aspect("equal", "box")
+	tight_layout()
+	gcf()
+end
+
+# ╔═╡ 0cd6cd76-5f6e-11eb-0bf5-2f0ea61ef29b
+md"# loop over θs"
+
+# ╔═╡ 5bbe8438-5f41-11eb-3d16-716bcb25400b
+begin
+	function run_θ_study(θ::Float64, nb_sims::Int)
+		# run sims, keep track of true vs pred and (k, λ)
+		a = Float64[] # true adsorption
+		â = Float64[] # pred adsorption
+		k = Int[]     # rank
+		λ = Float64[] # reg param
+		for s = 1:nb_sims
+			# run sim
+			test_result, opt_valid_run = run_simulation(θ)
+			a = vcat(a, test_result.a)
+			â = vcat(â, test_result.â)
+			push!(k, opt_valid_run.hyper_param.k)
+			push!(λ, opt_valid_run.hyper_param.λ)
+		end
+		return a, â, k, λ
+	end
+
+	a, â, k, λ = run_θ_study(0.4, 1)
+end
+
+# ╔═╡ 15c50a00-5f42-11eb-2044-91cd5ccf6d67
+md"
+stack vectors together to get the true distribution over different #'s of simulations.
+"
+
+# ╔═╡ b329a5f4-5f46-11eb-2e06-e9e991991815
+begin
+	# dist'n of hyper-params
+	fig, axs = subplots(2, 1)
+	for kᵢ in ks
+		axs[1].plot([kᵢ, kᵢ], [0, sum(k .== kᵢ)], color="C0", lw=3)
+		axs[1].scatter(kᵢ, sum(k .== kᵢ), color="C0")
+	end
+	axs[1].set_xlabel(L"dimensionality of latent space, $k$")
+	axs[1].set_ylabel("# simulations")
+	axs[1].set_xticks(ks)
+	
+	for λᵢ in λs
+		axs[2].plot([λᵢ, λᵢ], [0, sum(λ .== λᵢ)], color="C1", lw=3)
+		axs[2].scatter(λᵢ, sum(λ .== λᵢ), color="C1")
+	end
+	axs[2].set_xlabel(L"regularization parameter, $\lambda$")
+	axs[2].set_ylabel("# simulations")
+	axs[2].set_xticklabels(ks)
+	axs[2].set_xscale("log")
+	
+	# ax.set_yscale("log")
+	# ax.set_ylabel(L"$\lambda$")
+	# ax.set_yticks(1:length(λs))
+	tight_layout()
+	suptitle("hyper-parameter distribution")
+	gcf()
+end
+
+# ╔═╡ f32146b2-5f5f-11eb-019a-0b5838c2fd40
+
 
 # ╔═╡ Cell order:
 # ╠═92083d94-5b82-11eb-2274-ed62139bbf2d
@@ -401,12 +635,15 @@ end
 # ╠═a21ac3b8-5ba1-11eb-2d70-bdf4b395f563
 # ╟─1d363324-5b8f-11eb-2c78-6980a0d5f110
 # ╠═2366b3cc-5b8f-11eb-07e4-61cbc97d5480
+# ╟─6236b296-5f60-11eb-3aa7-5188433b3906
+# ╠═5ae47630-5f64-11eb-39f8-654f8d277674
 # ╟─6ef474cc-5b90-11eb-2fe2-87bc7b48f5b7
 # ╠═8a3c55ae-5ba4-11eb-3354-f9a8feaa7e91
 # ╠═1d745ea2-5ba5-11eb-274a-4ff7a65b0cb6
 # ╠═cc771288-5ba6-11eb-1792-9dfc54c58a8c
 # ╠═f7158eea-5ba4-11eb-17cc-d57c477cec39
 # ╠═8152d710-5b90-11eb-39f5-45d81aa298ab
+# ╠═21995e36-5f69-11eb-0a95-13d0136099df
 # ╟─e2dd554c-5baf-11eb-1b49-654d19bedecc
 # ╠═168d4c44-5bb3-11eb-2d16-af69f613b625
 # ╠═36da7c1e-5bb1-11eb-2bc9-438dabcf9cc5
@@ -416,4 +653,18 @@ end
 # ╟─2009751a-5bae-11eb-158f-a3d9cb98fe24
 # ╠═09b74408-5baa-11eb-3735-e9333756f565
 # ╠═b285e2fe-5ba7-11eb-2e12-83e72bcafa2f
-# ╠═a6b959aa-5bb2-11eb-3ab4-eb90f14f6f61
+# ╟─5d38b414-5f41-11eb-14b9-73a9007fc263
+# ╠═4f81a520-5f6d-11eb-1960-9918ca4f25e9
+# ╠═68fe93ae-5f6e-11eb-012a-81378cd15b41
+# ╠═53585188-5f6f-11eb-0fc0-abbd20ee33fe
+# ╠═74068408-5f70-11eb-02ba-417e847034c4
+# ╠═8548a48c-5f73-11eb-3d4f-550078ec546a
+# ╟─b0560c02-5f80-11eb-338b-c9cc48b741af
+# ╠═ba8ce81e-5f80-11eb-3e39-f942cb6d0d1f
+# ╠═c6caaa48-5f7f-11eb-3853-fdffcd51b2d5
+# ╠═59a72a22-5f82-11eb-1424-0913e7830bc4
+# ╟─0cd6cd76-5f6e-11eb-0bf5-2f0ea61ef29b
+# ╠═5bbe8438-5f41-11eb-3d16-716bcb25400b
+# ╟─15c50a00-5f42-11eb-2044-91cd5ccf6d67
+# ╠═b329a5f4-5f46-11eb-2e06-e9e991991815
+# ╠═f32146b2-5f5f-11eb-019a-0b5838c2fd40
